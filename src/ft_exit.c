6eba9ee97824ee0566f79298070c21d5d2c5ee64
맹주영 <mjy9088@naver.com>
2022-05-19 16:11:34 +0900

    enhance ft_exit

--- src/ft_exit.c
+++ src/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 14:10:15 by jmaing            #+#    #+#             */
-/*   Updated: 2022/05/19 16:05:12 by Juyeong Maing    ###   ########.fr       */
+/*   Updated: 2022/05/19 16:09:27 by Juyeong Maing    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,31 @@ static void	x(bool execute, void (*cleanup)())
 		cleanup_func();
 }
 
+#ifdef EXIT_PROHIBITED
+# ifdef EXIT_ALLOWED
+#  error both EXIT_ALLOWED and EXIT_PROHIBITED is defined
+# else
+
+int	ft_exit(signed char status)
+{
+	x(true, NULL);
+	return ((int) status);
+}
+
+# endif
+#else
+# ifdef EXIT_ALLOWED
+
 int	ft_exit(signed char status)
 {
 	x(true, NULL);
 	exit((int) status);
 }
+
+# else
+#  error EXIT_ALLOWED or EXIT_PROHIBITED must be defined
+# endif
+#endif
 
 void	ft_set_exit_handler(void (*cleanup)())
 {
