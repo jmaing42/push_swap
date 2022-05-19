c5d257f90522bde46f38121dd8f876531c52d822
맹주영 <mjy9088@naver.com>
2022-05-19 15:52:42 +0900

    add ft_strict_atoi

---
+++ src/ft_strict_atoi.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strict_atoi.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/19 15:43:21 by Juyeong Maing     #+#    #+#             */
+/*   Updated: 2022/05/19 15:44:12 by Juyeong Maing    ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_STRICT_ATOI_H
+# define FT_STRICT_ATOI_H
+
+# include "ft_types.h"
+
+t_err	ft_strict_atoi(const char *str, int *out);
+
+#endif
