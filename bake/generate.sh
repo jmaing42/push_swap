b02fbd4165c9953aed5891a13fe4b33e74087c9e
맹주영 <mjy9088@naver.com>
2022-07-06 00:20:26 +0900

    hardcode up to 7 items

--- bake/generate.sh
+++ bake/generate.sh
@@ -8,22 +8,24 @@ npm run build
 
 mkdir -p generated
 
-node dist/index.js ps_hardcoded_tst 1 tst > generated/ps_hardcoded_tst_1.c.txt
-node dist/index.js ps_hardcoded_tsb 1 tsb > generated/ps_hardcoded_tsb_1.c.txt
-node dist/index.js ps_hardcoded_txt 1 txt > generated/ps_hardcoded_txt_1.c.txt
-node dist/index.js ps_hardcoded_txb 1 txb > generated/ps_hardcoded_txb_1.c.txt
-node dist/index.js ps_hardcoded_tot 1 tot > generated/ps_hardcoded_tot_1.c.txt
-node dist/index.js ps_hardcoded_tos 1 tos > generated/ps_hardcoded_tos_1.c.txt
-node dist/index.js ps_hardcoded_tob 1 tob > generated/ps_hardcoded_tob_1.c.txt
-node dist/index.js ps_hardcoded_sss 1 sss > generated/ps_hardcoded_sss_1.c.txt
-node dist/index.js ps_hardcoded_sxs 1 sxs > generated/ps_hardcoded_sxs_1.c.txt
-node dist/index.js ps_hardcoded_sot 1 sot > generated/ps_hardcoded_sot_1.c.txt
-node dist/index.js ps_hardcoded_sos 1 sos > generated/ps_hardcoded_sos_1.c.txt
-node dist/index.js ps_hardcoded_sob 1 sob > generated/ps_hardcoded_sob_1.c.txt
-node dist/index.js ps_hardcoded_bst 1 bst > generated/ps_hardcoded_bst_1.c.txt
-node dist/index.js ps_hardcoded_bsb 1 bsb > generated/ps_hardcoded_bsb_1.c.txt
-node dist/index.js ps_hardcoded_bxt 1 bxt > generated/ps_hardcoded_bxt_1.c.txt
-node dist/index.js ps_hardcoded_bxb 1 bxb > generated/ps_hardcoded_bxb_1.c.txt
-node dist/index.js ps_hardcoded_bot 1 bot > generated/ps_hardcoded_bot_1.c.txt
-node dist/index.js ps_hardcoded_bos 1 bos > generated/ps_hardcoded_bos_1.c.txt
-node dist/index.js ps_hardcoded_bob 1 bob > generated/ps_hardcoded_bob_1.c.txt
+seq 7 | while read line; do
+  node dist/index.js ps_hardcoded_tst "$line" tst > "generated/ps_hardcoded_tst_$line.c.txt"
+  node dist/index.js ps_hardcoded_tsb "$line" tsb > "generated/ps_hardcoded_tsb_$line.c.txt"
+  node dist/index.js ps_hardcoded_txt "$line" txt > "generated/ps_hardcoded_txt_$line.c.txt"
+  node dist/index.js ps_hardcoded_txb "$line" txb > "generated/ps_hardcoded_txb_$line.c.txt"
+  node dist/index.js ps_hardcoded_tot "$line" tot > "generated/ps_hardcoded_tot_$line.c.txt"
+  node dist/index.js ps_hardcoded_tos "$line" tos > "generated/ps_hardcoded_tos_$line.c.txt"
+  node dist/index.js ps_hardcoded_tob "$line" tob > "generated/ps_hardcoded_tob_$line.c.txt"
+  node dist/index.js ps_hardcoded_sss "$line" sss > "generated/ps_hardcoded_sss_$line.c.txt"
+  node dist/index.js ps_hardcoded_sxs "$line" sxs > "generated/ps_hardcoded_sxs_$line.c.txt"
+  node dist/index.js ps_hardcoded_sot "$line" sot > "generated/ps_hardcoded_sot_$line.c.txt"
+  node dist/index.js ps_hardcoded_sos "$line" sos > "generated/ps_hardcoded_sos_$line.c.txt"
+  node dist/index.js ps_hardcoded_sob "$line" sob > "generated/ps_hardcoded_sob_$line.c.txt"
+  node dist/index.js ps_hardcoded_bst "$line" bst > "generated/ps_hardcoded_bst_$line.c.txt"
+  node dist/index.js ps_hardcoded_bsb "$line" bsb > "generated/ps_hardcoded_bsb_$line.c.txt"
+  node dist/index.js ps_hardcoded_bxt "$line" bxt > "generated/ps_hardcoded_bxt_$line.c.txt"
+  node dist/index.js ps_hardcoded_bxb "$line" bxb > "generated/ps_hardcoded_bxb_$line.c.txt"
+  node dist/index.js ps_hardcoded_bot "$line" bot > "generated/ps_hardcoded_bot_$line.c.txt"
+  node dist/index.js ps_hardcoded_bos "$line" bos > "generated/ps_hardcoded_bos_$line.c.txt"
+  node dist/index.js ps_hardcoded_bob "$line" bob > "generated/ps_hardcoded_bob_$line.c.txt"
+done
--- dev/main.c
+++ dev/main.c
@@ -6,7 +6,7 @@
 /*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 20:07:50 by Juyeong Maing     #+#    #+#             */
-/*   Updated: 2022/07/05 00:17:27 by Juyeong Maing    ###   ########.fr       */
+/*   Updated: 2022/07/06 00:18:50 by Juyeong Maing    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,165 @@
 
 #include "../src/ps/strategy/ps_strategy.h"
 
-#define COUNT 4242
+#define COUNT 1000
 
 int	main(void)
 {
 	t_ps_strategy_count	table[COUNT + 1];
 
-	table[0].tst.moves = 0; table[1].tst.moves = 0; table[2].tst.moves = 1;
-	table[0].txt.moves = 0; table[1].txt.moves = 0; table[2].txt.moves = 1;
-	table[0].tsb.moves = 0; table[1].tsb.moves = 1; table[2].tsb.moves = 3;
-	table[0].txb.moves = 0; table[1].txb.moves = 1; table[2].txb.moves = 3;
-	table[0].tot.moves = 0; table[1].tot.moves = 1; table[2].tot.moves = 3;
-	table[0].tos.moves = 0; table[1].tos.moves = 1; table[2].tos.moves = 3;
-	table[0].tob.moves = 0; table[1].tob.moves = 2; table[2].tob.moves = 5;
-	table[0].bst.moves = 0; table[1].bst.moves = 1; table[2].bst.moves = 3;
-	table[0].bxt.moves = 0; table[1].bxt.moves = 1; table[2].bxt.moves = 3;
-	table[0].bsb.moves = 0; table[1].bsb.moves = 0; table[2].bsb.moves = 5;
-	table[0].bxb.moves = 0; table[1].bxb.moves = 0; table[2].bxb.moves = 5;
-	table[0].bot.moves = 0; table[1].bot.moves = 2; table[2].bot.moves = 5;
-	table[0].bos.moves = 0; table[1].bos.moves = 2; table[2].bos.moves = 5;
-	table[0].bob.moves = 0; table[1].bob.moves = 3; table[2].bob.moves = 7;
-	table[0].sss.moves = 0; table[1].sss.moves = 0; table[2].sss.moves = 1;
-	table[0].sxs.moves = 0; table[1].sxs.moves = 0; table[2].sxs.moves = 1;
-	table[0].sot.moves = 0; table[1].sot.moves = 1; table[2].sot.moves = 3;
-	table[0].sos.moves = 0; table[1].sos.moves = 1; table[2].sos.moves = 3;
-	table[0].sob.moves = 0; table[1].sob.moves = 2; table[2].sob.moves = 5;
-	for (size_t i = 3; i <= COUNT; i++)
+	table[0].bob.moves = 0;
+	table[1].bob.moves = 3;
+	table[2].bob.moves = 6;
+	table[3].bob.moves = 9;
+	table[4].bob.moves = 12;
+	table[5].bob.moves = 15;
+	table[6].bob.moves = 18;
+	table[7].bob.moves = 22;
+	table[0].bos.moves = 0;
+	table[1].bos.moves = 2;
+	table[2].bos.moves = 4;
+	table[3].bos.moves = 7;
+	table[4].bos.moves = 9;
+	table[5].bos.moves = 12;
+	table[6].bos.moves = 14;
+	table[7].bos.moves = 17;
+	table[0].bot.moves = 0;
+	table[1].bot.moves = 2;
+	table[2].bot.moves = 4;
+	table[3].bot.moves = 7;
+	table[4].bot.moves = 9;
+	table[5].bot.moves = 13;
+	table[6].bot.moves = 16;
+	table[7].bot.moves = 19;
+	table[0].bsb.moves = 0;
+	table[1].bsb.moves = 0;
+	table[2].bsb.moves = 5;
+	table[3].bsb.moves = 9;
+	table[4].bsb.moves = 13;
+	table[5].bsb.moves = 17;
+	table[6].bsb.moves = 21;
+	table[7].bsb.moves = 25;
+	table[0].bst.moves = 0;
+	table[1].bst.moves = 1;
+	table[2].bst.moves = 3;
+	table[3].bst.moves = 6;
+	table[4].bst.moves = 9;
+	table[5].bst.moves = 13;
+	table[6].bst.moves = 16;
+	table[7].bst.moves = 20;
+	table[0].bxb.moves = 0;
+	table[1].bxb.moves = 1;
+	table[2].bxb.moves = 5;
+	table[3].bxb.moves = 9;
+	table[4].bxb.moves = 13;
+	table[5].bxb.moves = 17;
+	table[6].bxb.moves = 21;
+	table[7].bxb.moves = 25;
+	table[0].bxt.moves = 0;
+	table[1].bxt.moves = 1;
+	table[2].bxt.moves = 3;
+	table[3].bxt.moves = 6;
+	table[4].bxt.moves = 9;
+	table[5].bxt.moves = 13;
+	table[6].bxt.moves = 16;
+	table[7].bxt.moves = 19;
+	table[0].sob.moves = 0;
+	table[1].sob.moves = 2;
+	table[2].sob.moves = 4;
+	table[3].sob.moves = 7;
+	table[4].sob.moves = 9;
+	table[5].sob.moves = 12;
+	table[6].sob.moves = 14;
+	table[7].sob.moves = 17;
+	table[0].sos.moves = 0;
+	table[1].sos.moves = 1;
+	table[2].sos.moves = 3;
+	table[3].sos.moves = 5;
+	table[4].sos.moves = 7;
+	table[5].sos.moves = 9;
+	table[6].sos.moves = 11;
+	table[7].sos.moves = 13;
+	table[0].sot.moves = 0;
+	table[1].sot.moves = 1;
+	table[2].sot.moves = 3;
+	table[3].sot.moves = 5;
+	table[4].sot.moves = 7;
+	table[5].sot.moves = 9;
+	table[6].sot.moves = 11;
+	table[7].sot.moves = 13;
+	table[0].sss.moves = 0;
+	table[1].sss.moves = 0;
+	table[2].sss.moves = 1;
+	table[3].sss.moves = 2;
+	table[4].sss.moves = 5;
+	table[5].sss.moves = 8;
+	table[6].sss.moves = 11;
+	table[7].sss.moves = 14;
+	table[0].sxs.moves = 0;
+	table[1].sxs.moves = 0;
+	table[2].sxs.moves = 1;
+	table[3].sxs.moves = 2;
+	table[4].sxs.moves = 5;
+	table[5].sxs.moves = 8;
+	table[6].sxs.moves = 10;
+	table[7].sxs.moves = 13;
+	table[0].tob.moves = 0;
+	table[1].tob.moves = 2;
+	table[2].tob.moves = 4;
+	table[3].tob.moves = 7;
+	table[4].tob.moves = 9;
+	table[5].tob.moves = 13;
+	table[6].tob.moves = 16;
+	table[7].tob.moves = 19;
+	table[0].tos.moves = 0;
+	table[1].tos.moves = 1;
+	table[2].tos.moves = 3;
+	table[3].tos.moves = 5;
+	table[4].tos.moves = 7;
+	table[5].tos.moves = 9;
+	table[6].tos.moves = 11;
+	table[7].tos.moves = 13;
+	table[0].tot.moves = 0;
+	table[1].tot.moves = 1;
+	table[2].tot.moves = 3;
+	table[3].tot.moves = 6;
+	table[4].tot.moves = 8;
+	table[5].tot.moves = 10;
+	table[6].tot.moves = 13;
+	table[7].tot.moves = 16;
+	table[0].tsb.moves = 0;
+	table[1].tsb.moves = 1;
+	table[2].tsb.moves = 3;
+	table[3].tsb.moves = 6;
+	table[4].tsb.moves = 9;
+	table[5].tsb.moves = 13;
+	table[6].tsb.moves = 16;
+	table[7].tsb.moves = 20;
+	table[0].tst.moves = 0;
+	table[1].tst.moves = 0;
+	table[2].tst.moves = 1;
+	table[3].tst.moves = 5;
+	table[4].tst.moves = 9;
+	table[5].tst.moves = 12;
+	table[6].tst.moves = 15;
+	table[7].tst.moves = 18;
+	table[0].txb.moves = 0;
+	table[1].txb.moves = 1;
+	table[2].txb.moves = 3;
+	table[3].txb.moves = 6;
+	table[4].txb.moves = 9;
+	table[5].txb.moves = 13;
+	table[6].txb.moves = 16;
+	table[7].txb.moves = 19;
+	table[0].txt.moves = 0;
+	table[1].txt.moves = 0;
+	table[2].txt.moves = 1;
+	table[3].txt.moves = 5;
+	table[4].txt.moves = 9;
+	table[5].txt.moves = 11;
+	table[6].txt.moves = 14;
+	table[7].txt.moves = 17;
+	for (size_t i = 8; i <= COUNT; i++)
 	{
 		ps_strategy_bake(table, i, i + 1);
 		printf("%4zu: ", i);
