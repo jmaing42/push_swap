4e7946b19cb60f6ea2ff7d1a41c93ea3d9ef48cc
Juyeong Maing <mjy9088@naver.com>
2022-07-08 18:36:34 +0900

    fix typo

--- src/ps/strategy/tst/merge_rotate_no_rotate/ps_strategy_tst_merge_no_rotate_count.c
+++ src/ps/strategy/tst/merge_rotate_no_rotate/ps_strategy_tst_merge_no_rotate_count.c
@@ -17,7 +17,7 @@
 #include "ft_size_t.h"
 #include "ps_strategy_util.h"
 
-size_t	ps_strategy_tst_merge_late_rotate_count(
+size_t	ps_strategy_tst_merge_no_rotate_count(
 	const t_ps_strategy_count *table,
 	size_t x,
 	size_t y,
@@ -32,7 +32,7 @@ size_t	ps_strategy_tst_merge_late_rotate_count(
 	return (sort_x + sort_y + sort_z + collect);
 }
 
-void	ps_strategy_tst_merge_late_rotate_count_init_if_better(
+void	ps_strategy_tst_merge_no_rotate_count_init_if_better(
 	t_ps_strategy_count *table,
 	size_t index
 )
@@ -41,12 +41,12 @@ void	ps_strategy_tst_merge_late_rotate_count_init_if_better(
 		= ps_strategy_util_try_all_permutations(
 			table,
 			index,
-			ps_strategy_tst_merge_late_rotate_count);
+			ps_strategy_tst_merge_no_rotate_count);
 
 	if (table[index].tst.item.moves > current.moves)
 	{
 		table[index].tst.item = current;
-		table[index] = ps_strategy_tst_merge_late_rotate_execute;
-		table[index] = "tst_merge_late_rotate";
+		table[index] = ps_strategy_tst_merge_no_rotate_execute;
+		table[index] = "tst_merge_no_rotate";
 	}
 }
