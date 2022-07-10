a29463bedf2a275dda3fab3d508cd3ed945f3a90
맹주영 <mjy9088@naver.com>
2022-07-10 17:45:08 +0900

    prevent divide by zero

--- src/checker_init.c
+++ src/checker_init.c
@@ -6,7 +6,7 @@
 /*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/11 21:59:55 by Juyeong Maing     #+#    #+#             */
-/*   Updated: 2022/06/14 00:53:50 by Juyeong Maing    ###   ########.fr       */
+/*   Updated: 2022/07/10 17:44:58 by Juyeong Maing    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@ t_checker_state	*checker_init(size_t count, char **arr_as_string)
 {
 	t_checker_state *const	result
 		= (t_checker_state *)ft_malloc(sizeof(t_checker_state));
-	int *const				a = (int *)ft_malloc(sizeof(int) * count);
-	int *const				b = (int *)ft_malloc(sizeof(int) * count);
+	int *const				a = (int *)ft_malloc(sizeof(int) * (count + 1));
+	int *const				b = (int *)ft_malloc(sizeof(int) * (count + 1));
 	size_t					i;
 	size_t					j;
 
-	result->a = (t_checker_state_part){a, count, 0, count};
-	result->b = (t_checker_state_part){b, count, 0, 0};
+	result->a = (t_checker_state_part){a, count + 1, 0, count};
+	result->b = (t_checker_state_part){b, count + 1, 0, 0};
 	i = -1;
 	while (++i < count)
 	{
