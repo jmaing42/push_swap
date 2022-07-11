22c615fb4e6d668e25a4365ea993f2bc0b183ffe
Juyeong Maing <mjy9088@naver.com>
2022-07-11 17:48:37 +0900

    WIP: add reduce

--- src/ps/stream/ps_stream_internal_reduce.c
+++ src/ps/stream/ps_stream_internal_reduce.c
@@ -10,9 +10,9 @@ size_t	ps_stream_internal_reduce_push(
 )
 {
 	size_t	min;
-	if (!self->tail || self->tail->value.type != PS_STREAM_NODE_TYPE_SEPARATOR)
-		return (0);
 
+	if (!self->tail || *self->tail->value.type != PS_STREAM_NODE_TYPE_SEPARATOR)
+		return (0);
 	min = self->tail->value.separator->push_to_right;
 	if (to_b)
 		min = self->tail->value.separator->push_to_left;
@@ -22,5 +22,33 @@ size_t	ps_stream_internal_reduce_push(
 	else
 		self->tail->value.separator->push_to_right -= min;
 	ps_stream_internal_clear_last_separator_node(self);
+	return (min);
+}
+
+size_t	ps_stream_internal_reduce_swap(
+	t_ps_stream *self,
+	size_t count,
+	bool to_b
+)
+{
+	size_t	min;
+
+// TODO: implement
+	if (
+		!self->tail
+		|| *self->tail->value.type != PS_STREAM_NODE_TYPE_PARALLEL
+		|| (!to_b && !self->tail->value.parallel->a_tail)
+		|| (to_b && !self->tail->value.parallel->b_tail))
+		return (0);
+	if (!to_b)
+		min = self->tail->value.parallel->a_tail->count;
+	else
+		min = self->tail->value.parallel->b_tail->count;
+	min = ft_size_t_min(min, count);
+	if (!to_b)
+		self->tail->value.parallel->a_tail->count -= min;
+	else
+		self->tail->value.parallel->b_tail->count -= min;
+	ps_stream_internal_clear_last_parallel_node(self);
 	return (min);
 }
