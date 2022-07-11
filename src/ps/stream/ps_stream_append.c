03465dca85cedb6ba7c770fc2b7785b8b493c09f
맹주영 <mjy9088@naver.com>
2022-07-11 23:31:56 +0900

    fix bug

--- src/ps/stream/ps_stream_append.c
+++ src/ps/stream/ps_stream_append.c
@@ -6,12 +6,13 @@
 /*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 08:59:14 by Juyeong Maing     #+#    #+#             */
-/*   Updated: 2022/07/11 23:28:05 by Juyeong Maing    ###   ########.fr       */
+/*   Updated: 2022/07/11 23:31:38 by Juyeong Maing    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ps_stream.h"
 #include "ps_stream_internal.h"
+
+#include "ft_size_t.h"
 
 static t_err	push(t_ps_stream *self, size_t count, bool to_b)
 {
@@ -19,6 +20,10 @@ static t_err	push(t_ps_stream *self, size_t count, bool to_b)
 
 	if ((to_b && !self->a) || (!to_b && !self->b))
 		return (false);
+	if (!to_b)
+		count = ft_size_t_min(count, self->b);
+	else
+		count = ft_size_t_min(count, self->a);
 	remain = count
 		- ps_stream_internal_reduce_push(self, count, to_b);
 	return (
@@ -41,10 +46,13 @@ static t_err	swap(t_ps_stream *self, size_t count, bool to_b)
 {
 	size_t	remain;
 
-	if ((to_b && self->a < 2) || (!to_b && self->b < 2))
+	if ((to_b && self->a < 2) || (!to_b && self->b < 2) || count % 2 == 0)
 		return (false);
 	remain = count
 		- ps_stream_internal_reduce_swap(self, count, to_b);
+	remain = remain % 2;
+	if (!remain)
+		return (false);
 	return (
 		(
 			(
