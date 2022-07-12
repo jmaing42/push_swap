bbdc681a13e61be33b2ca6272f23328fe951f817
맹주영 <mjy9088@naver.com>
2022-07-12 21:53:07 +0900

    use common test script

--- src/ps/strategy/util/test/try_all_permutations/test.sh
+++ src/ps/strategy/util/test/try_all_permutations/test.sh
@@ -8,3 +8,27 @@ set -e
 ./test.exe 3 | cmp data/3.txt
 ./test.exe 4 | cmp data/4.txt
 ./test.exe 5 | cmp data/5.txt
+
+find data -name "*.txt" | sed s/\\.txt\$// | sort | cut -c 6- | while IFS= read -r line
+do
+  if ./test.exe $line | cmp "data/$line.txt" ; then
+    if [ "$V" = "1" ]; then
+      if [ -t 1 ]; then
+        printf "\033[0;32m[PASS]\033[0m "
+      else
+        printf "[PASS] "
+      fi
+      printf "ps_strategy_util/try_all_permutations - %s\n" "$line"
+    fi
+  else
+    if [ "$V" = "1" ]; then
+      if [ -t 1 ]; then
+        printf "\033[0;31m[FAIL]\033[0m "
+      else
+        printf "[FAIL] "
+      fi
+      printf "ps_strategy_util/try_all_permutations - %s\n" "$line"
+    fi
+    exit 1
+  fi
+done
