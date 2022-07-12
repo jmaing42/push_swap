16bda432b2372f8b6440b4795fd7041f5bb5018e
맹주영 <mjy9088@naver.com>
2022-07-12 09:29:37 +0900

    enhance test script

--- src/ps/stream/test/basic/test.sh
+++ src/ps/stream/test/basic/test.sh
@@ -4,13 +4,24 @@ set -e
 
 find data -name "*.out.txt" | sed s/\\.out\\.txt\$// | cut -c 6- | while IFS= read -r line
 do
-  ./test.exe < "data/$line.in.txt" | cmp "data/$line.out.txt"
-  if [ "$V" = "1" ]; then
-    if [ -t 1 ]; then
-      printf "\033[0;32m[PASS]\033[0m "
-    else
-      printf "[PASS] "
+  if ./test.exe < "data/$line.in.txt" | cmp -s "data/$line.out.txt" ; then
+    if [ "$V" = "1" ]; then
+      if [ -t 1 ]; then
+        printf "\033[0;32m[PASS]\033[0m "
+      else
+        printf "[PASS] "
+      fi
+      printf "ps_stream/basic - %s\n" "$line"
     fi
-    printf "ps_stream/basic - %s\n" "$line"
+  else
+    if [ "$V" = "1" ]; then
+      if [ -t 1 ]; then
+        printf "\033[0;31m[FAIL]\033[0m "
+      else
+        printf "[FAIL] "
+      fi
+      printf "ps_stream/basic - %s\n" "$line"
+    fi
+    exit 1
   fi
 done
