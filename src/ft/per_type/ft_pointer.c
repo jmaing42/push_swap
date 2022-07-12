6b39c084f8f4f4d0ee3a23f7b6370a4962691e6e
맹주영 <mjy9088@naver.com>
2022-07-12 22:08:23 +0900

    fix incorrect include

--- src/ft/per_type/ft_pointer.c
+++ src/ft/per_type/ft_pointer.c
@@ -6,11 +6,11 @@
 /*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 19:44:45 by Juyeong Maing     #+#    #+#             */
-/*   Updated: 2022/06/25 19:49:40 by Juyeong Maing    ###   ########.fr       */
+/*   Updated: 2022/07/12 22:07:17 by Juyeong Maing    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_const_pointer.h"
+#include "ft_pointer.h"
 
 void	*ft_pointer_if(
 	bool condition,
