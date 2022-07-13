7c819893e56ca7a7bb27e7dcc6b865b19ea8f574
Juyeong Maing <mjy9088@naver.com>
2022-07-13 11:00:58 +0900

    change ps_stream internal prototype

--- src/ps/stream/ps_stream_internal.h
+++ src/ps/stream/ps_stream_internal.h
@@ -15,23 +15,6 @@
 
 # include "ps_stream.h"
 
-size_t	ps_stream_internal_reduce_push(
-			t_ps_stream *self,
-			size_t count,
-			bool to_b);
-size_t	ps_stream_internal_reduce_swap(
-			t_ps_stream *self,
-			size_t count,
-			bool to_b);
-size_t	ps_stream_internal_reduce_rotate(
-			t_ps_stream *self,
-			size_t count,
-			bool to_b);
-size_t	ps_stream_internal_reduce_reverse_rotate(
-			t_ps_stream *self,
-			size_t count,
-			bool to_b);
-
 bool	ps_stream_internal_clear_last_node(
 			t_ps_stream_node *self);
 bool	ps_stream_internal_clear_last_node_separator(
@@ -40,18 +23,27 @@ bool	ps_stream_internal_clear_last_node_parallel(
 			t_ps_stream_node_parallel *self);
 bool	ps_stream_internal_clear_last_parallel_node(
 			t_ps_stream_node_parallel_list *node);
+bool	ps_stream_internal_clear_last_parallel_node_swap(
+			t_ps_stream_node_parallel_list *node);
+bool	ps_stream_internal_clear_last_parallel_node_rotate(
+			t_ps_stream_node_parallel_list *node);
+bool	ps_stream_internal_clear_last_parallel_node_reverse_rotate(
+			t_ps_stream_node_parallel_list *node);
 
-t_err	ps_stream_internal_append_no_check_pa(t_ps_stream *self, size_t count);
-t_err	ps_stream_internal_append_no_check_pb(t_ps_stream *self, size_t count);
-t_err	ps_stream_internal_append_no_check_sa(t_ps_stream *self);
-t_err	ps_stream_internal_append_no_check_sb(t_ps_stream *self);
-t_err	ps_stream_internal_append_no_check_ss(t_ps_stream *self);
-t_err	ps_stream_internal_append_no_check_ra(t_ps_stream *self, size_t count);
-t_err	ps_stream_internal_append_no_check_rb(t_ps_stream *self, size_t count);
-t_err	ps_stream_internal_append_no_check_rr(t_ps_stream *self, size_t count);
-t_err	ps_stream_internal_append_no_check_rra(t_ps_stream *self, size_t count);
-t_err	ps_stream_internal_append_no_check_rrb(t_ps_stream *self, size_t count);
-t_err	ps_stream_internal_append_no_check_rrr(t_ps_stream *self, size_t count);
+t_err	ps_stream_internal_append_no_check_push_to_a(
+			t_ps_stream *self,
+			size_t count);
+t_err	ps_stream_internal_append_no_check_push_to_b(
+			t_ps_stream *self,
+			size_t count);
+t_err	ps_stream_internal_append_no_check_swap(
+			t_ps_stream_node_parallel_list *node);
+t_err	ps_stream_internal_append_no_check_rotate(
+			t_ps_stream_node_parallel_list *node,
+			size_t count);
+t_err	ps_stream_internal_append_no_check_reverse_rotate(
+			t_ps_stream_node_parallel_list *node,
+			size_t count);
 
 t_err	ps_stream_internal_append_pa(t_ps_stream *self, size_t count);
 t_err	ps_stream_internal_append_pb(t_ps_stream *self, size_t count);
