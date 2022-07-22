e97a56d2bdca24890f814116df0eb38eb5c364b0
맹주영 <mjy9088@naver.com>
2022-07-22 21:43:58 +0900

    fix incorrect dependency

--- src/ps/solve/util/array/ps_solve_util_array_sort.c
+++ src/ps/solve/util/array/ps_solve_util_array_sort.c
@@ -6,7 +6,7 @@
 /*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 20:20:31 by Juyeong Maing     #+#    #+#             */
-/*   Updated: 2022/07/19 20:20:31 by Juyeong Maing    ###   ########.fr       */
+/*   Updated: 2022/07/22 21:43:25 by Juyeong Maing    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,32 +17,4 @@ void	ps_solve_util_array_sort(
 )
 {
 	ps_solve_util_sort(self->array, self->length);
-}
-
-void	ps_solve_util_array_inverse(
-	t_ps_solve_util_array *self
-)
-{
-	size_t	i;
-
-	i = -1;
-	while (++i < self->length)
-		self->array[i] = ~self->array[i];
-}
-
-void	ps_solve_util_array_reverse(
-	t_ps_solve_util_array *self
-)
-{
-	const size_t	to = self->length / 2;
-	size_t			i;
-	int				tmp;
-
-	i = -1;
-	while (++i < to)
-	{
-		tmp = self->array[self->length - i - 1];
-		self->array[self->length - i - 1] = self->array[i];
-		self->array[i] = tmp;
-	}
 }
