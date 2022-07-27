7bb4f0eeb327067824a1b481d5b6e42d506d0a72
맹주영 <mjy9088@naver.com>
2022-07-28 08:55:31 +0900

    fix incorrect name

--- src/ps/solve/util/divide/ps_solve_util_divide_from_top_sbt.c
+++ src/ps/solve/util/divide/ps_solve_util_divide_from_top_sbt.c
@@ -6,13 +6,13 @@
 /*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/24 09:01:52 by Juyeong Maing     #+#    #+#             */
-/*   Updated: 2022/07/24 09:22:09 by Juyeong Maing    ###   ########.fr       */
+/*   Updated: 2022/07/28 08:54:37 by Juyeong Maing    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ps_solve_internal.h"
 
-t_err	ps_solve_util_divide_from_top_stb(
+t_err	ps_solve_util_divide_from_top_sbt(
 	const t_ps_solve_context *context,
 	const t_ps_solve_util_parts *parts,
 	const int *array,
