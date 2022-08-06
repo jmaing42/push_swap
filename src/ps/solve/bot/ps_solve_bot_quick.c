7ad20d77bc20df59ddf474ead5f49a8a6163d98e
맹주영 <mjy9088@naver.com>
2022-08-06 22:55:26 +0900

    fix incorrect name

--- src/ps/solve/bot/ps_solve_bot_quick.c
+++ src/ps/solve/bot/ps_solve_bot_quick.c
@@ -6,7 +6,7 @@
 /*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/06 21:53:02 by Juyeong Maing     #+#    #+#             */
-/*   Updated: 2022/08/06 22:27:00 by Juyeong Maing    ###   ########.fr       */
+/*   Updated: 2022/08/06 22:54:15 by Juyeong Maing    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 #include <stdlib.h>
 
-t_err	ps_solve_bst_quick_solve(
+t_err	ps_solve_bot_quick_solve(
 	const t_ps_solve_context *context,
 	int *arr,
 	t_ps_solve_count_size size,
@@ -36,7 +36,7 @@ t_err	ps_solve_bst_quick_solve(
 	return (result);
 }
 
-size_t	ps_solve_bst_quick_count(
+size_t	ps_solve_bot_quick_count(
 	const t_ps_solve_context *context,
 	size_t x,
 	size_t y,
