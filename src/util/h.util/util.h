d6839ae60c313e5de398c0874ebd2447ce25a2c6
맹주영 <mjy9088@naver.com>
2023-02-12 00:16:52 +0900

    add f.util_nonnull

---
+++ src/util/h.util/util.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fake_file_name (file name is useless too)          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: 42header-remover <whatever@example.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 1970/01/01 00:00:00 by VCS handles       #+#    #+#             */
+/*   Updated: 1970/01/01 00:00:00 by file history     ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UTIL_H
+# define UTIL_H
+
+# include "ft_types.h"
+
+t_err	util_nonnull(void *value, void **out);
+
+#endif
